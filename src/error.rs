5c95bda4aaa84ff9f662e985823de4504b3fee07
Acizza <jonathanmce@pm.me>
2019-01-08 12:36:03 -0800

    [error] Fix spacing inbetween ConfigError variants

--- src/error.rs
+++ src/error.rs
@@ -4,14 +4,19 @@ use failure::Fail;
 pub enum ConfigError {
     #[fail(display = "failed to get config directory")]
     FailedToGetConfigDir,
+    
     #[fail(display = "failed to read config file")]
     FailedToRead(#[cause] std::io::Error),
+    
     #[fail(display = "failed to parse config file")]
     FailedToParse(#[cause] toml::de::Error),
+    
     #[fail(display = "failed to serialize config")]
     FailedToSerialize(#[cause] toml::ser::Error),
+
     #[fail(display = "failed to write config to file")]
     FailedToWrite(#[cause] std::io::Error),
+
     #[fail(display = "failed to create config dir")]
     FailedToCreateConfigDir(#[cause] std::io::Error),
 }
