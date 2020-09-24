c06ff46b221e08df3da5e9919a023c02ce488be8
Acizza <jgit@tuta.io>
2020-09-24 11:04:12 -0700

    Implement prefix list prototype
    This allows you to select a prefix from the (temporary) list.

--- src/ui/PrefixList/PrefixList.tsx
+++ src/ui/PrefixList/PrefixList.tsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./PrefixList.module.scss";
 
 function PrefixList(): JSX.Element {
+  const [selected, setSelected] = useState<number | undefined>();
+
   return (
     <div className={styles.panel}>
-      <span>Prefix Panel</span>
+      {[...Array(10)].map((_, i) => (
+        <Prefix
+          key={i}
+          index={i}
+          selected={selected}
+          onClick={() => setSelected(i)}
+        />
+      ))}
     </div>
+  );
+}
+
+interface PrefixProps {
+  index: number;
+  selected: number | undefined;
+  onClick?: (event: React.MouseEvent) => void;
+}
+
+function Prefix(props: PrefixProps): JSX.Element {
+  const selectedClass =
+    props.selected === props.index ? styles.prefixSelected : "";
+
+  const classes = `${styles.prefix} ${selectedClass}`;
+
+  return (
+    <span className={classes} onClick={props.onClick}>
+      Prefix {props.index}
+    </span>
   );
 }
 
