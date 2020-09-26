890350d3239077a7e0f509d9419921e719c620da
Acizza <jgit@tuta.io>
2020-09-25 17:41:58 -0700

    Add close button to error modal

--- src/ui/ErrorModal.tsx
+++ src/ui/ErrorModal.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 import styles from "./ErrorModal.module.scss";
 
 export interface Error {
@@ -6,10 +8,21 @@ export interface Error {
   message: string;
 }
 
-function ErrorModal(props: Error): JSX.Element {
+function ErrorModal(props: Error): JSX.Element | null {
+  const [open, setOpen] = useState(true);
+
+  if (!open) return null;
+
   return (
     <div className={styles.errorModal}>
-      <span className={styles.context}>{props.context}</span>
+      <div className={styles.header}>
+        <span className={styles.context}>{props.context}</span>
+        <FontAwesomeIcon
+          className={styles.closeIcon}
+          icon={faTimes}
+          onClick={() => setOpen((prev) => !prev)}
+        />
+      </div>
       <span className={styles.message}>{props.message}</span>
     </div>
   );
