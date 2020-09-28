3320df50f1a0758177111f2cd482e1701d9be9a9
Acizza <jgit@tuta.io>
2020-09-28 16:59:37 -0700

    Display package name & version from package.json in header component

--- src/ui/SidePanel/Header.tsx
+++ src/ui/SidePanel/Header.tsx
@@ -2,11 +2,16 @@ import { faCog } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import styles from "./Header.module.scss";
+// @ts-ignore: This import produces an error, but works perfectly when ignored...
+import { name, version } from "../../../package.json";
 
 function Header(): JSX.Element {
   return (
     <div className={styles.header}>
-      <span>wpfxm</span>
+      <div>
+        <span>{name}</span>
+        <span className={styles.version}>{version}</span>
+      </div>
       <FontAwesomeIcon className={styles.settingsIcon} icon={faCog} />
     </div>
   );
